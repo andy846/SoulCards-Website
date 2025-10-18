0de26800fa262d08e56554fa055dd59eb6b44d16
andy846 <andy846@gmail.com>
2025-10-18 14:11:37 +0800

    Fix base path for local development and production

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import tsconfigPaths from 'vite-tsconfig-paths'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), tsconfigPaths()],
-  base: '/SoulCards-Website/',
+  base: process.env.NODE_ENV === 'production' ? '/SoulCards-Website/' : '/',
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
